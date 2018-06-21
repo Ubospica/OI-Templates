ff0a25c9a4eab85154723de102a42ab6c754e308
Ubospica <32952380+Ubospica@users.noreply.github.com>
2018-06-21 16:40:14 +0800

    Add files via upload

---
+++ templates/template.cpp
@@ -0,0 +1,40 @@
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<set>
+#include<map>
+using namespace std;
+#define rep(i,l,r) for(register int i=(l);i<=(r);++i)
+#define repdo(i,l,r) for(register int i=(l);i>=(r);--i)
+#define il inline
+typedef double db;
+typedef long long ll;
+
+//---------------------------------------
+int n;
+int main(){
+	ios::sync_with_stdio(0),cin.tie(0);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//	freopen(".in","r",stdin);
+//	freopen(".out","w",stdout);
+
+
+
+
